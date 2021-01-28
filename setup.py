d2af79befab1c9b6bf129942ebd4a4552fe714ea
jesopo <github@lolnerd.net>
2021-01-28 23:59:20 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name="twisted-opm",
+    version="0.1",
+    author="Marien Zwart, jesopo",
+    author_email="txopm@lolnerd.net",
+    url="https://github.com/jesopo/twisted-opm",
+    description="Twisted-based BOPM-like open proxy scanner.",
+    license="MIT",
+    packages=["opm", "twisted/plugins"]
+)
