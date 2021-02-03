acabfd947c1406564c9ed74011aa97baca9e9538
Eric Mertens <emertens@gmail.com>
2021-02-02 21:23:10 -0800

    update buildsystem

--- setup.py
+++ setup.py
@@ -10,6 +10,13 @@ setup(
     url="https://github.com/jesopo/twisted-opm",
     description="Twisted-based BOPM-like open proxy scanner.",
     license="MIT",
-    packages=["opm", "twisted/plugins"],
-    python_requires=">=3.5"
+    packages=["opm", "opm/plugins", "twisted/plugins"],
+    python_requires=">=3.5",
+    install_requires=[
+        'cryptography>=3.1',
+        'ircchallenge==0.1.1',
+        'pyyaml      >=5.3.1',
+        'Twisted     >=20.3.0',
+        'pyOpenSSL   >=20.0.1',
+    ]
 )
